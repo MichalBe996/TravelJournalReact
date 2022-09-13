38b667fa362ee1cb68f0bb1019e459935fcb5386
MichalBe996 <83282995+MichalBe996@users.noreply.github.com>
2022-09-13 14:42:15 +0200

    Update travelData.js

--- src/components/travelData.js
+++ src/components/travelData.js
@@ -5,6 +5,7 @@ export default [
         country: "Norway",
         name: "Trolltunga",
         date: "12.01.2021-24.01.2021",
+        googleLocation: "https://goo.gl/maps/FbGXVrTEKrGhh33LA",
         description: "Trolltunga (troll tongue) is a rock formation situated about 1,100 metres (3,600 ft) above sea level in Ullensvang Municipality in Vestland county, Norway. The cliff juts horizontally out from the mountain, about 700 metres (2,300 ft) above the north side of the lake Ringedalsvatnet."
 
     },
@@ -15,6 +16,7 @@ export default [
         country: "Peru",
         name: "Machu Picchu",
         date: "13.12.2021-21.12.2021",
+        googleLocation: "https://goo.gl/maps/c6qaz2rhHniQHmu7A",
         description: "Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a 2,430-meter (7,970 ft) mountain ridge. It is located in the Machupicchu District within Urubamba Province above the Sacred Valley, which is 80 kilometers (50 mi) northwest of Cusco. The Urubamba River flows past it, cutting through the Cordillera and creating a canyon with a tropical mountain climate."
     },
     {
@@ -23,6 +25,7 @@ export default [
         country: "Peru",
         name: "Nazca lines",
         date: "03.04.2022-10.04.2022",
+        googleLocation: "https://goo.gl/maps/mvpu6MiL1RLuNDmR7",
         description: "The Nazca Lines are a group of geoglyphs made in the soil of the Nazca Desert in southern Peru. They were created between 500 BCE and 500 CEby  people making depressions or shallow incisions in the desert floor, removing pebbles and leaving differently colored dirt exposed."
     },
     {
@@ -31,6 +34,7 @@ export default [
         country: "Italy",
         name: "Pompeii",
         date: "05.05.2022-14.05.2022",
+        googleLocation: "https://goo.gl/maps/tCKX2ZDbAh3F15KM7",
         description: "Pompeii was an ancient city located in what is now the comune of Pompei near Naples in the Campania region of Italy. Pompeii, along with Herculaneum and many villas in the surrounding area (e.g. at Boscoreale, Stabiae), was buried under 4 to 6 m (13 to 20 ft) of volcanic ash and pumice in the eruption of Mount Vesuvius in AD 79. "
     }
 ]
